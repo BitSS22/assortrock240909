7096b1e6fa0a355c84cf798c75050fab5cc443ce
Bitss <donrang@naver.com>
2024-09-23 16:51:20 +0900

    신동민 : NumberCount 문제 풀이

--- 24.09.23. NumberCount/source.cpp
+++ 24.09.23. NumberCount/source.cpp
@@ -23,23 +23,27 @@ int NumberCount(int _Value)
 void NumberToString(char* Buffer, int BufferSize, int _Value)
 {
     int count = NumberCount(_Value);
-    if (2 > BufferSize || count + 1 > BufferSize)
-        assert(nullptr);
 
-    if (!_Value)
-    {
-        Buffer[0] = '0';
-        Buffer[1] = 0;
-    }
+    if (2 > BufferSize|| count + 1 > BufferSize)
+        assert(nullptr);
 
     Buffer[count] = 0;
 
-    while (_Value)
+    if (0 == _Value)
+    {
+        Buffer[0] = '0';
+        Buffer[1] = 0;
+        return;
+    }
+
+    while (count)
     {
         --count;
-        Buffer[count] = (_Value % 10) + '0';
+        Buffer[count] = _Value % 10 + '0';
         _Value /= 10;
     }
+
+    return;
 }
 
 int main()
@@ -56,9 +60,11 @@ int main()
     char Buffer[100] = {3,45,1,23,52,5,4,6,23,4,4,25,3,4,4};
 
     NumberToString(Buffer, 100, 3000);
-    //NumberToString(Buffer, 100, 0);
-    //NumberToString(Buffer, 100, 52342341);
-    //NumberToString(Buffer, 5, 3712);
+    NumberToString(Buffer, 100, 0);
+    NumberToString(Buffer, 100, 52342341);
+    NumberToString(Buffer, 5, 3712);
+    // NumberToString(Buffer, 4, 7345);
+    NumberToString(Buffer, 1, 0);
 
 
     return 0;
